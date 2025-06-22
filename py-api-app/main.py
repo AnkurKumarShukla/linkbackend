7511a41bda3ba6ebeb1a86cb4214519b9a626904
AnkurKumarShukla <shuklaankur123456789@gmail.com>
2025-06-22 22:48:50 +0530

    server database issue resolved

--- py-api-app/main.py
+++ py-api-app/main.py
@@ -12,8 +12,9 @@ import os
 
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
-DB_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "data_stream/db", "stream_data.db"))  
-DATABASE_URL = f"sqlite:///{DB_PATH}"
+DB_PATH = "/app/db/stream_data.db"
+DATABASE_URL = "sqlite:////app/db/stream_data.db"
+
 
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
